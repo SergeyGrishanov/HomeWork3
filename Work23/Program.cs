57ff0d534653b530ebe9c1777c6642bbe02a74e7
Sergey <blackwhiterainbow@yandex.ru>
2023-01-23 07:54:41 +0300

    add таблица кубов от 1 до N

---
+++ Work23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+while (num >= count)
+{
+    Console.Write($"{Math.Pow(count,3)}");
+    if (count != num) Console.Write(", ");
+    count++;
+}
