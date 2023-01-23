f7f3c5f94dc499fd3d54bd445446c66de9e46e4c
Sergey <blackwhiterainbow@yandex.ru>
2023-01-23 07:53:57 +0300

    add Расстояние между точками координат

---
+++ Work21/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+
+Console.WriteLine("Введите x1: ");
+int x1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите y1: ");
+int y1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите z1: ");
+int z1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите x2: ");
+int x2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите y2: ");
+int y2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите z2: ");
+int z2 = int.Parse(Console.ReadLine()!);
+
+double num = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
+
+Console.WriteLine($"{num:F2}");
